84beef49db969df5bc60660e67bbab24b304e956
Tania Antunes <estt3609@ipt.pt>
2019-04-05 19:43:03 +0100

    Criação das classes que identificarão as tabelas da BD no SqlServer

---
+++ Multas2/Multas2/Models/Agentes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas2.Models
+{
+    public class Agentes
+    {
+        /// atributos:
+        /// - nº agente
+        /// - nome
+        /// - esquadra
+        /// - foto
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Esquadra { get; set; }
+
+        public string Foto { get; set; }
+
+
+    }
+}
---
+++ Multas2/Multas2/Models/MultasDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas2.Models
+{
+    public class MultasDB :DbContext
+    {
+        public MultasDB () : base("MultasDBConnectionString") { }
+        // Definir as tabelas
+        public DbSet<Condutores> Condutores { get; set; }
+        public DbSet<Viaturas> Carros { get; set; }
+        public DbSet<Agentes> Agentes { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+
+
+    }
+}
