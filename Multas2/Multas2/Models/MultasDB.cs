2f3fb0ca0144c651dab99646a024684ecc7e212c
Tania Antunes <estt3609@ipt.pt>
2019-05-03 17:59:02 +0100

    Adição da instrução para eliminar a opção "on delete cascade"

--- Multas2/Multas2/Models/MultasDB.cs
+++ Multas2/Multas2/Models/MultasDB.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace Multas2.Models
 {
-    public class MultasDB :DbContext
+    public class MultasDB : DbContext
     {
-        public MultasDB () : base("MultasDBConnectionString") { }
+        public MultasDB() : base("MultasDBConnectionString") { }
         // Definir as tabelas
 
         // vamos colocar, aqui, as instruções relativas às tabelas do 'negócio'
         // descrever os nomes das tabelas na Base de Dados
         public DbSet<Condutores> Condutores { get; set; } //Tabela Condutores
-        public DbSet<Viaturas> Carros { get; set; } //Tabela Viaturas
+        public DbSet<Viaturas> Viaturas { get; set; } //Tabela Viaturas
         public DbSet<Agentes> Agentes { get; set; } //Tabela Agentes
         public DbSet<Multas> Multas { get; set; } // Tabela Multas
 
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
+
 }
